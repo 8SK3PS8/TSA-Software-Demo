55fb2ada7e03c08b8fca887062dcee7424e68a9d
Aryanpatra <aryanpatra75@gmail.com>
2021-02-14 00:19:44 -0600

    enter

--- .github/google-custom-search.js
+++ .github/google-custom-search.js
@@ -1,20 +1,13 @@
-
-var SerpWow = require('google-search-results-serpwow')
-
-// create the serpwow object, passing in our API key
-let serpwow = new SerpWow('BC275EC4A95A4E7AACE7207C84F88307')
-
-// set up the location query parameters
-var params = {
-  q: 'london'
-}
-
-// retrieve the location query results as JSON
-serpwow.locations(params)
-  .then(result => {
-    // pretty-print the JSON result
-    console.log(JSON.stringify(result, 0, 2));
-  })
-  .catch(error => {
-    console.log(error);
-  });
+'use strict';
+ 
+const yelp = require('yelp-fusion');
+const client = yelp.client('3DBbiFi8MwR7IcfA6gz76KEjTewdNMkyCG_7-jmu6aaNqYvOlhAeo0b-4ohwkQlWd1jVAYH6PMPLBv1i9MEoHLUJoMvp-OFeOMwwqSO4wcV067WqGbXajuOMmrgoYHYx');
+ 
+client.search({
+  term: 'Italian',
+  location: 'Houston, tx',
+}).then(response => {
+  console.log(response.jsonBody.businesses[0].name);
+}).catch(e => {
+  console.log(e);
+});
